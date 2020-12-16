6633d18c40217c130fabc709e36e7eb57f6fa0f1
Sadia <Sa284@zips.uakron.edu>
2020-12-15 23:17:27 -0500

    initial commit

---
+++ src/components/Burger/BurgetIngredients/BurgerIngredients.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const burgerIngredient =() =>{
+    let ingredient= null;
+    
+
+};
+
+export default burgerIngredient;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+
+import React, {Fragment, Component} from "react";
+
+
+class BurgerBuilder extends Component{
+    render() {
+        return (
+            <Fragment>
+                <div>Burger</div>
+                <div>Burger Controls</div>
+            </Fragment>
+        );
+    }
+}
+export default BurgerBuilder;
